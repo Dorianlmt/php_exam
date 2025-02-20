8d81b4eeb4d504597c6bbab0e0760b996642f6c0
SamuelMorisson <morissonsamuel@gmail.com>
2025-02-20 12:33:38 +0100

    Update: profile page front-end done

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+//#[IsGranted("ROLE_USER")] // Protection : seul un utilisateur connecté peut voir son profil
 final class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
     public function index(): Response
     {
+        /*$user = $this->getUser(); // Récupérer l'utilisateur connecté
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login'); // Rediriger vers la connexion si non connecté
+        }*/
+
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            //'user' => $user,
         ]);
     }
 }
