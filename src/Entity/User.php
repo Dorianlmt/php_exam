22bba6b55550ad365973d97788ff06f17fdf14e6
SamuelMorisson <morissonsamuel@gmail.com>
2025-02-23 18:34:48 +0100

    Fix: admin can now access to account page

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -102,9 +102,16 @@ class User implements PasswordAuthenticatedUserInterface, UserInterface
         return $this;
     }
 
+    // public function getRoles(): array
+    // {
+    //     return $this->roles;
+    // }
     public function getRoles(): array
     {
-        return $this->roles;
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
     public function setRoles(array $roles): static
