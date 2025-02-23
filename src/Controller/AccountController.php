edc48a68ce625f29255cb8598c84dccba708cc06
SamuelMorisson <morissonsamuel@gmail.com>
2025-02-23 18:41:18 +0100

    Update: Ajout de la page admin, mentions légales et à propos par Damien, mise en commun sur la branche Samuel

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted("ROLE_USER")]
+final class AccountController extends AbstractController
+{
+    #[Route('/account', name: 'app_account')]
+    public function index(): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('account/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
